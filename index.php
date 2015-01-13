d822eb0ed43e1a602162b8d02553d00761c291a6
WietzeMulder <wietzem@gmail.com>
2015-01-13 15:50:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ session_start();
 include('pages/functions.inc.php');
 
 $page = getStrIfIsset($_GET['p'], "index");
+echo $page;
 
 if(!isset($_SESSION['forumAdmin'])){
 	$_SESSION['forumAdmin'] = 0;
