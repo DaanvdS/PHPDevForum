1149414e58793988bde659d2f73b795ea2883972
WietzeMulder <wietzem@gmail.com>
2015-01-13 12:01:27 +0100

    Push from Wietze (13 Jan 2015 11:01:24)

--- pages/activate.inc.php
+++ pages/activate.inc.php
@@ -1,12 +1,15 @@
 <?php
-//forum Muziektheater, authored by Wietze Mulder and Daan van der Spek
-//Not to be copied without written permission from the owners
-if(isset($_GET['c']) && isset($_GET['u'])){
+// Forum Informatica, authored by Wietze Mulder and Daan van der Spek
+// Not to be copied without written permission from the owners
+
+if(isset($_GET['c']) && isset($_GET['u'])) {
 	include("dbconnect.inc.php");
-	$MySQL['result']= $MySQL['connection']->query("SELECT `id` FROM `users` WHERE `activationcode` = '".$_GET['c']."' AND `activated` = '0' AND `id` = '".$_GET['u']."' LIMIT 1");
+	$MySQL['query'] = "SELECT `id` FROM `users` WHERE `activationcode` = '".$_GET['c']."' AND `activated` = '0' AND `id` = '".$_GET['u']."' LIMIT 1";
+	$MySQL['result'] = $MySQL['connection']->query($MySQL['query']);
 	if($MySQL['result']->num_rows==1){
-		$MySQL['result']= $MySQL['connection']->query("UPDATE `users` SET `activated` = '1', `activationcode` = '0' WHERE  `id` = '".$_GET['u']."'");
-		if($MySQL['connection']->affected_rows==1){
+		$MySQL['query'] = "UPDATE `users` SET `activated` = '1', `activationcode` = '0' WHERE  `id` = '".$_GET['u']."'";
+		$MySQL['result'] = $MySQL['connection']->query($MySQL['query']);
+		if($MySQL['connection']->affected_rows==1) {
 			echo "<p>Account activated succesfully.</p>";
 		} else {
 			echo "<p>Something went wrong when activating the account.</p>";
@@ -14,4 +17,8 @@ if(isset($_GET['c']) && isset($_GET['u'])){
 	} else {
 		echo "<p>Account not found or it has already been activated.</p>";
 	}
-} else { echo "<p>Not all the information required was specified.</p>"; }
+}
+else {
+	echo "<p>Not all the information required was specified.</p>"; 
+}
+?>
