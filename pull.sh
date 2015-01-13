b81c2d83a1902addff172e10b7bbe204c07b4b99
Daan van der Spek <daanvanderspek@gmail.com>
2015-01-13 23:39:49 +0100

    Push from Daan (13 Jan 2015 23:39:49)

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash    
-echo "password" | sudo forum
-cd ~
-git pull -q  
+echo "password" | su
+cd /home/daan/public_html/forum/PHPDevForum
+runuser -l forum -c 'git pull -q'
 echo 'Done!'
