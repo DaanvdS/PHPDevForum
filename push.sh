ce85da4c9500f0470836fbf042fdcaaf2f5075af
WietzeMulder <wietzem@gmail.com>
2015-01-13 12:10:10 +0100

    Push from Wietze (13 Jan 2015 11:10:06)

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash          
 date=$(date +"%d %b %Y %X")
 user=$1
-git pull
+echo 'git pull'
+git pull -q  
+echo 'git add'
 git add .
-git commit -m "Push from $user ($date)"
-git push origin master
+echo 'git commit'
+git commit -q -m "Push from $user ($date)"
+echo 'git push'
+git push -q origin master
+echo 'git done'
