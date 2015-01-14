75d2e001773d62806070867442e4a9656397633d
Daan van der Spek <daanvanderspek@gmail.com>
2015-01-14 23:24:37 +0100

    Push from Daan (14 Jan 2015 23:24:36)

--- pages/adminfunctions.inc.php
+++ pages/adminfunctions.inc.php
@@ -84,7 +84,7 @@ function adminShowGroupPanel(){
 			$MySQL['result'] = $MySQL['connection']->query($MySQL['query']);
 			redirectIfDone($MySQL['connection'], "Changed succesfully", "adminpanel&section=groupmanagement");
 		} elseif($_GET['action'] == "new"){
-			$MySQL['query'] = "INSERT INTO `usergroups` (`name`) VALUES (".getIfIssetGet('name', '').")";
+			$MySQL['query'] = "INSERT INTO `usergroups` (`name`) VALUES ('".getIfIssetGet('name', '')."')";
 			echo $MySQL['query'];
 			$MySQL['connection']->query($MySQL['query']) or die(mysqli_error($MySQL['connection']));
 			redirectIfDone($MySQL['connection'], "Added succesfully", "adminpanel&section=groupmanagement");
