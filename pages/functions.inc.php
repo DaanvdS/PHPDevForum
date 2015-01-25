dea72868169e8d255133d0f7d11a44c691fdd442
WietzeMulder <wietzem@gmail.com>
2015-01-25 18:40:32 +0100

    Push from Wietze (25 Jan 2015 06:40:31 PM)

--- pages/functions.inc.php
+++ pages/functions.inc.php
@@ -102,7 +102,7 @@ function hasSpecialRights($id,$op){
 
 function isInGroup($id,$groupid){
 	include("dbconnect.inc.php");
-	$MySQL['query'] = "SELECT * FROM usersInGroups WHERE userID='".$id."' groupID='".$groupid."'";
+	$MySQL['query'] = "SELECT * FROM usersInGroups WHERE userID = '".$id."' groupID = '".$groupid."'";
 	echo $MySQL['query'];
 	$MySQL['result'] = $MySQL['connection']->query($MySQL['query']) or die(mysqli_error($MySQL['connection']));
 	if ($MySQL['result']->num_rows > 0){
