3d93d9d44b20deb1bf9ec17ffa9e3502fe7fd972
WietzeMulder <wietzem@gmail.com>
2015-01-25 20:05:26 +0100

    Push from Wietze (25 Jan 2015 08:05:26 PM)

--- pages/ptb.inc.php
+++ pages/ptb.inc.php
@@ -198,7 +198,7 @@ function ptbAction(){
 		case 'save': 
 			logAction();
 			if($_GET['ptb']=='t'){$sticky=$_GET['sticky'];}else{$sticky=0;}
-			ptbChgSav(getIfIssetGet('ptb', ''), getIfIssetGet('id', ''), array(getIfIssetGet('data', ''),$sticky), getIfIssetGet('return', ''), getIfIssetGet('pag', ''));
+			ptbChgSav(getIfIssetGet('ptb', ''), getIfIssetGet('id', ''), array(getIfIssetGet('data', ''),$sticky,$_GET['board_id']), getIfIssetGet('return', ''), getIfIssetGet('pag', ''));
 			break;
 		case 'change': 
 			ptbChgForm(getIfIssetGet('ptb', ''), getIfIssetGet('id', ''), getIfIssetGet('return', ''), getIfIssetGet('pag', ''));
