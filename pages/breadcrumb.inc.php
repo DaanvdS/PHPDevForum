591ebd900ac703600c34b79871d7ff1722d04ba0
Daan van der Spek <daanvanderspek@gmail.com>
2015-01-30 13:03:55 +0100

    Push from Daan (30 Jan 2015 12:03:51)

--- pages/breadcrumb.inc.php
+++ pages/breadcrumb.inc.php
@@ -34,8 +34,7 @@ if(isset($_GET['p'])){
 		$out.="<a class='hidden-a' href='?p=".$_GET['p']."'>".ucwords($_GET['p'])."</a>";
 	}
 }else {
-	$out = $index " > Index";
-	$index = "";
+	$out = " > <a class='hidden-a' href='?p=index'>Index</a>";
 }
 echo "<p id='breadcrumbP' style='overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>".$out."</p>";
 echo "
