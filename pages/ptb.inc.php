46dce94f5d58edefa9b7295bb55dec794572515b
Daan van der Spek <daanvanderspek@gmail.com>
2015-03-03 11:25:24 +0100

    Push from Daan (03 Mar 2015 10:25:21)

--- pages/ptb.inc.php
+++ pages/ptb.inc.php
@@ -116,7 +116,7 @@ function ptbChgSav($ptb, $id, $data, $return, $pag){
 	}*/
 	$columns = implode (",", $columns);
 	$values = implode (",", $values);
-  	$MySQL['query'] = "REPLACE INTO `".$ptb[0]."` SET ".$colums." VALUES (".$values.") WHERE `id` = ".$id;
+  	$MySQL['query'] = "REPLACE INTO `".$ptb[0]."` SET ".$columns." VALUES (".$values.") WHERE `id` = ".$id;
 	echo $MySQL['query'];
 	/*$MySQL['connection']->query($MySQL['query']) or die(mysqli_error($MySQL['connection']));
 	if($MySQL['connection']->affected_rows == 1){
