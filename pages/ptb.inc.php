827898b3714c63f1076a79ea9f7a0ed0ff3b5d02
Daan van der Spek <daanvanderspek@gmail.com>
2015-03-03 12:08:23 +0100

    Push from Daan (03 Mar 2015 11:08:21)

--- pages/ptb.inc.php
+++ pages/ptb.inc.php
@@ -60,7 +60,7 @@ function ptbChgSav($ptb, $id, $data, $return, $pag){
  	} elseif(count($columns) == 3) {
  		$fin_update=$columns[0].' = '.$values[0].', '.$columns[1].' = '.$values[1].', '.$columns[2].' = '.$values[2];
 	}
-  	$MySQL['query'] = "INSERT INTO `".$ptb[0]."` (`id`,".$columnsT.") VALUES (".$id.",".$valuesT.") ON DUIPLICATE KEY
+  	$MySQL['query'] = "INSERT INTO `".$ptb[0]."` (`id`,".$columnsT.") VALUES (".$id.",".$valuesT.") ON DUPLICATE KEY
 		UPDATE ".$fin_update."
 	";
 	echo $MySQL['query'];
